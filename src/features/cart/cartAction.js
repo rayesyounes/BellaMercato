99224bd6b6a92204300054a07af99f63856b3e38
Spoutnikrs <rayesyounes88@gmail.com>
2024-01-25 03:12:43 +0100

    add to cart

--- src/features/cart/cartAction.js
+++ src/features/cart/cartAction.js
@@ -142,5 +142,45 @@ const removeItem = createAsyncThunk("cart/removeItem", async ({userId, productId
     }
 });
 
+const addToCart = createAsyncThunk("cart/addToCart", async ({userId, productId}, {dispatch, getState}) => {
+    try {
+        dispatch(setLoading(true));
 
-export {fetchUserCart, decreaseQuantity, increaseQuantity, removeItem};
+        const {products} = getState().products;
+        const response = await axios.get(`${API_BASE_URL}/carts`);
+        const userCart = response.data.find((cart) => cart.user_id === userId);
+        const productToAdd = products.find((product) => product.id === productId);
+
+        if (userCart && productToAdd) {
+            const itemToUpdate = userCart.items.find((item) => item.product_id === productId);
+
+            if (itemToUpdate) {
+                itemToUpdate.quantity += 1;
+            } else {
+                userCart.items.push({product_id: productId, quantity: 1});
+            }
+
+            let total = 0;
+            userCart.items.forEach((item) => {
+                const product = products.find((product) => product.id === item.product_id);
+                if (product) {
+                    total += product.price * item.quantity;
+                }
+            });
+
+            const updatedCart = {...userCart, total};
+            await axios.put(`${API_BASE_URL}/carts/${userCart.id}`, updatedCart).then(() => {
+                dispatch(setCart(updatedCart));
+            });
+        }
+
+        dispatch(setLoading(false));
+    } catch (error) {
+        dispatch(setLoading(false));
+        dispatch(setError(error.message));
+        throw error;
+    }
+});
+
+
+export {fetchUserCart, decreaseQuantity, increaseQuantity, removeItem, addToCart};
