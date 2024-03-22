eee1b6ecd65277eef68cd9251acf6880ca9f18af
Spoutnikrs <rayesyounes88@gmail.com>
2024-03-22 16:17:15 +0000

    update api url

--- src/features/users/usersAction.js
+++ src/features/users/usersAction.js
@@ -14,7 +14,7 @@ import {
 } from "./usersSlice";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://bellamercato-api.vercel.app";
 
 export const getUsersAsync = () => async (dispatch) => {
     try {
